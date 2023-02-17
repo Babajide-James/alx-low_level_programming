b07baba18a72b259b16dcfc22951b558e270bc4b
Babajide-James <teestartaiwo121@gmail.com>
2023-02-17 15:13:46 -0800

    The last digit of c variable practice

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,9 +20,9 @@ int main(void)
 	/* your code goes there */
 	m = n % 10;
 	if (m > 5)
-		printf("last digit of %d is %d and is greater than 5\n", n, m);
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	if (m == 0)
-		printf("last digit of %d is %d amd is 0\n", n, m);
+		printf("Last digit of %d is %d amd is 0\n", n, m);
 	if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 not 0\n", n, m);
 	return (0);
