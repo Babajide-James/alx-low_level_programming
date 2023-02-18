f15475f6f1e2865e7c4b31fc70ec298cf6cc2546
Babajide-James <teestartaiwo121@gmail.com>
2023-02-17 18:01:31 -0800

    This program help print all single digit number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * This is to be used to print all digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
