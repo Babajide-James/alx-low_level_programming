58d360eebb3207163ea767c4a1cdde5dc37d2fff
Babajide-James <teestartaiwo121@gmail.com>
2023-02-19 23:46:18 -0800

    let's print our compilation doc.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,7 @@
  *
  * Return: result (0)
  */
+
 int main(void)
 {	
 	char v;
@@ -12,12 +13,12 @@ int main(void)
 	long long int y;
 	float z;
 
-	printf()
-	{
+	printf(
 		size of a char: %d;
 		size of an int: %d;
 		size of a long int: %d;
 		size of long long int: %d;
 		size of float: %d;
+		1, 4, 4, 8, 16
+	      );
 		return (0);
-	}
