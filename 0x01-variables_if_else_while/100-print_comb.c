3b829339d07ca67e3bf4e9b5ccca9e8b531408c8
Babajide-James <teestartaiwo121@gmail.com>
2023-02-19 19:33:46 -0800

    task 11

--- 0x01-variables_if_else_while/100-print_comb.c
+++ 0x01-variables_if_else_while/100-print_comb.c
@@ -4,6 +4,17 @@
  * main - Entry point
  *
  * Return: result 0
+ *
+ *
+ * Don't be discouraged
+ *
+ *
+ * It is getting serious
+ *
+ *
+ * i move
+ *
+ * All code should be in the main function
  */
 
 int main(void)
@@ -16,7 +27,7 @@ int main(void)
 		c = 0;
 		while (c < 10)
 		{
-		if else (d != c && d < c)
+		if (d != c && d < c)
 		{
 		putchar('0' + d);
 		putchar('0' + c);
