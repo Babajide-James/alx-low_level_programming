2f1c467cfb0c14c0125938384aadf0d8e289cbaa
Babajide-James <teestartaiwo121@gmail.com>
2023-02-20 00:38:58 -0800

    prints sizes of various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 printf("size of a char: %1u byte(s)\n", (unsigned long)sizeof(v));
 printf("size of an int: %1u byte(s)\n", (unsigned long)sizeof(w));
 printf("size of a long int: %1u byte(s)\n", (unsigned long)sizeof(x));
-printf("size of long long int: %1u byte(s)\n", (unsigned long)sizeof(d));
+printf("size of long long int: %1u byte(s)\n", (unsigned long)sizeof(y));
 printf("size of a float: %1u byte(s)\n", (unsigned long)sizeof(z));
 return (0);
 }
