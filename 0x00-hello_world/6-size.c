1a2877ed58460125f35405d4edcccec8c822384d
Babajide-James <teestartaiwo121@gmail.com>
2023-02-19 23:35:50 -0800

    size of various types on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: result (0)
+ */
+int main(void)
+{	
+	char v;
+	int w;
+	long int x;
+	long long int y;
+	float z;
+
+	printf()
+	{
+		size of a char: %d;
+		size of an int: %d;
+		size of a long int: %d;
+		size of long long int: %d;
+		size of float: %d;
+		return (0);
+	}
