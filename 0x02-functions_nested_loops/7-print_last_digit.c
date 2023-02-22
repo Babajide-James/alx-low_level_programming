a0e24e0f18f49982e19e5fd57c9621cc09d1e75e
Babajide-James <teestartaiwo121@gmail.com>
2023-02-22 10:03:54 -0800

    used to print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,15 @@
  * Return value of the last digit number
  */
 
-int print_last_digit(int 0)
+int print_last_digit(int n)
+
 {
 	int last;
 
 	last = n % 10;
 	if (last < 0)
 	{
-	last * -1;
+	last = last * -1;
 	}
 	_putchar(last + '0');
 	return (last);
