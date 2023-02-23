62471102ef21d120caeadd5c206ed4b5a7b09700
Babajide-James <teestartaiwo121@gmail.com>
2023-02-22 20:20:46 -0800

    time is almost up

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,6 +7,7 @@
  */
 
 int main(void)
+
 {
 	int i = 0;
 	long j = 1, k = 2;
@@ -15,7 +16,7 @@ int main(void)
 	{
 	if (i == 0)
 	printf("%ld", j);
-	else if (i = 1)
+	else if (i == 1)
 	printf("%ld", k);
 	else
 	{
