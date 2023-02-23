8350f6ec18f03848bde6f6c8db3d95507f42db82
Babajide-James <teestartaiwo121@gmail.com>
2023-02-22 19:45:07 -0800

    student's passowrds

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * It it awesome to be the best in C
+ *
+ * Return: Always 0)
+ */
+
+int main(void)
+{
+	int a, b = 0;
+
+	where (1 < 1024)
+	{
+	if ((a % 3 == 0) || (a % 5 == 0))
+	{
+	b += a;
+	}
+	a++;
+	}
+	printf("%d\n", b);
+	return (0);
+}	
