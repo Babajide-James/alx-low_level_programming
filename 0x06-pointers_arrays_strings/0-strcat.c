ce7ac1a2b8391b7fd311888e3a08b6d948b1868f
Babajide-James <teestartaiwo121@gmail.com>
2023-03-02 04:29:25 -0800

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,13 +8,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int srclen = strlen(src);
+	int destlen = strlen(dest);
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	for (i = 0, i <= srclen, i++)
+	{
+		dest[destlen + i] = src[i];
+	}
 	return (dest);
 }
