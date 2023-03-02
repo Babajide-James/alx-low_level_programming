ea9168d70673b3512699db383bd1772519d14ddf
Babajide-James <teestartaiwo121@gmail.com>
2023-03-02 15:34:31 -0800

    my work

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,17 +5,20 @@
  * @dest: the first char
  * @src: the second char
  * @n: the integer
- * Return: Apointer to the resultig string dest
+ * Return: Apointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int destlen = 0;
+	int srclen = 0;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	for (i = 0; dest[i] != '\0'; i++)
+		destlen++;
+	for (i = 0; src[i] != '\0'; i++)
+		srclen++;
+	for (i = 0; i < n; i++)
+		dest[destlen + i] = src[i];
+	dest[i] = '\0';
 	return (dest);
 }
