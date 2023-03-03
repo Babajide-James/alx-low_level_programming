42b64b6fe6edc734f7a1e9a2209668f7e90ff3af
Babajide-James <teestartaiwo121@gmail.com>
2023-03-02 16:35:45 -0800

    my commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,16 +9,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 	int destlen = 0;
-	int srclen = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		destlen++;
-	for (i = 0; src[i] != '\0'; i++)
-		srclen++;
-	for (i = 0; i < n; i++)
-		dest[destlen + i] = src[i];
-	dest[i] = '\0';
+	for (i =  0, j = destlen;
+			i < n && dest[i] != '\0' && src[i] != '\0';
+			i++, j++)
+		dest[j] = src[i];
 	return (dest);
 }
