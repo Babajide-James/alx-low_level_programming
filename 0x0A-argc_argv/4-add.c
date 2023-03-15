6c82c6c02754167d58c96126adfb2b6361a93fa4
Babajide-James <teestartaiwo121@gmail.com>
2023-03-15 15:50:28 -0700

    not sure

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,4 +9,24 @@
  */
 int main(int argc, char *argv[])
 {
+	int x;
+	int sum = 0;
+
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		for (x = 1; x < argc; x++)
+		{
+			if (!atoi(argv[x]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum = sum + argv[x];
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
 }
