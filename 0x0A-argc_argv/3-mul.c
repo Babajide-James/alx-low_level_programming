9bd3acf3e9b44210f2a413b36b29ff2d3cd5159a
Babajide-James <teestartaiwo121@gmail.com>
2023-03-15 09:20:57 -0700

    always learn

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers
+ * @argc: the argument for task
+ * @argv: the string
+ * Return: Always 0 if success
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	int x, y, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		mul = x * y;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
