3380d4b918d495a4b71c9bc479facb695d075eff
Babajide-James <teestartaiwo121@gmail.com>
2023-03-17 02:23:09 -0700

    little changes

--- 0x01-variables_if_else_while/try.c
+++ 0x01-variables_if_else_while/try.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - entry point here
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -14,10 +14,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
+	if (n < 0)
 		printf("%d is negative\n", n);
+	else if (n > 0)
+		printf("%i is positive\n", (unsigned int)n);
 	else
 		printf("%d is zero\n", n);
 	return (0);
