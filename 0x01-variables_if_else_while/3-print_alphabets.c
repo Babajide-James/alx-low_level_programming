eaf2c50ca9e0601cadf833e6c33faa521e782f14
Babajide-James <teestartaiwo121@gmail.com>
2023-03-17 06:47:26 -0700

    betty styled

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
+
 /**
  * main - The entry point of my code
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 	char c;
-
 	char d;
 
 	c = 'a';
 	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'Z') {
-			putchar(d);
-			d++;
-		}
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
 	putchar('\n');
 	return (0);
 }
